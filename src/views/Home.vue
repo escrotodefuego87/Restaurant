1e35a5fbf53aa6ae4c0d8dc6424a920b421ec3f2
Dave <alexruedarivas@gmail.com>
2019-09-17 22:33:46 -0500

    minor changes at main page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,29 +5,54 @@
     Setup del proyecto, todo listo para empezar el desarrollo!
     <template v-slot:actions>
       <v-btn
-        @click="sayhi()"
+        @click="showCard()"
         text
         color="deep-purple accent-4"
       >
-        Hi!
+        Show card
       </v-btn>
+
     </template>
   </v-banner>
+
+  <v-card
+    max-width="344"
+    class="mx-auto"
+    v-if="flag==true"
+  >
+
+        <v-card-title>Title</v-card-title>
+        <v-card-text>Text text text text text text text text text text</v-card-text>
+        <v-card-actions>
+          <v-btn text @click="hideCard()">Dismiss</v-btn>
+        </v-card-actions>
+      </v-card>
 </v-app>
   
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
+
 
 export default {
+  data: function(){
+    return {
+      flag: false
+    }
+  },
   components: {
-    HelloWorld,
+    
+  },
+  mounted(){
+  
   },
   methods: {
 
-    sayhi: function(){
-      alert('Muestra');
+    showCard: function(){
+      this.flag=true;
+    },
+    hideCard: function(){
+      this.flag=false;
     }
 
   },
