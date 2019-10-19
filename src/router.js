37083313176950d9460d472079d78ff6f5ce5715
crasher9032 <tavodash@gmail.com>
2019-10-19 12:43:48 -0500

    Menu 1ra etapa

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Menu from "./components/menu.vue";
 
 Vue.use(Router);
 
@@ -12,6 +13,11 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
+    },
+    {
+      path: "/menu",
+      name: "Menu",
+      component: Menu
     },
     {
       path: "/about",
