053bcc0a2dc42fa509a15f1e61d8f584f71f84d0
Dave <alexruedarivas@gmail.com>
2019-10-20 22:31:02 -0500

    Segunda revision

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,14 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Restaurant</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://vuetifyjs.com/es-MX/getting-started/quick-start"
-        target="_blank"
-      >
-        <span class="mr-2">Vuetify documentation</span>
-      </v-btn>
-      <v-btn
-        text
-        href="https://vuejs.org/v2/guide/"
-        target="_blank"
-      >
-        <span class="mr-2">Vue.js documentation</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <Home />
-    </v-content>
+    <v-toolbar app fixed>
+      <v-toolbar-title>FOOD FOLKS</v-toolbar-title>
+      <div class="flex-grow-1"></div>
+      <v-toolbar-items>
+        <v-btn text>Log in</v-btn>
+        <v-btn text>Sign in</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <Home />
   </v-app>
 </template>
 
---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-row justify="center">
+        <v-dialog v-model="dialog" persistent max-width="300">
+            <v-card>
+                <v-card-title>
+                    <span class="headline">Log in</span>
+                </v-card-title>
+                <v-card-text>
+                   
+                </v-card-text>
+            </v-card>
+        </v-dialog>
+    </v-row>
+</template>
+<script>
+export default {
+    name: 'Login',
+    data: () => ({
+        dialog: true
+    })
+    
+}
+</script>
